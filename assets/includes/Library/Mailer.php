5774ae6056899cac19ddb05cebcf740704fdf3e4
Swarnadeep Karmakar <74227514+iamswarnadeep@users.noreply.github.com>
2022-08-15 18:52:45 +0530

    Added

--- assets/includes/Library/Mailer.php
+++ assets/includes/Library/Mailer.php
@@ -21,7 +21,7 @@ class Mailer {
 		$mail->CharSet="windows-1256";
 		$mail->Subject = iconv('windows-1256', 'utf-8', $title);
 
-		$mail->FromName = 'Pearls! Questions & Answers platform';
+		$mail->FromName = 'MyEnggPal Questions-Answers forum';
 		$mail->From = $from;
 		$mail->AddAddress($to,$to_name);
 
